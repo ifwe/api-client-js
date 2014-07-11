1352e4c6d5de1dab89c507cf50a3336609c5425c
npolkowske <npolkowske@tagged.com>
2014-07-11 16:19:21 -0700

    adding test files and main gitignore

--- test/lib/index_test.js
+++ test/lib/index_test.js
@@ -13,4 +13,24 @@ describe('Tagged API', function() {
     it('endpoint is defined', function() {
         this.api.should.have.property('endpoint');
     });
+
+    it('api should have a execute method', function() {
+        this.api.execute.should.be.a('function');
+    });
+
+    it('api.execute should throw exception with a method string', function() {
+        var _this = this;
+
+        expect(function(){
+           _this.api.execute(); 
+        }).to.throw();
+    });
+
+    it('api.execute should not throw exception with a method string', function() {
+        var _this = this;
+
+        expect(function(){
+           _this.api.execute("foo.bar"); 
+        }).to.not.throw();  
+    });
 });
