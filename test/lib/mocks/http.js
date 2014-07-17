9115b2a3b846dc03352a158e4f80ab697014a251
Hector Virgen <hvirgen@tagged.com>
2014-07-16 21:54:46 -0700

    Add support for resolving HTTP requests in the http mock

--- test/lib/mocks/http.js
+++ test/lib/mocks/http.js
@@ -1,5 +1,34 @@
+var Q = require('q');
+
 var HttpMock = function() {
-    this.post = sinon.spy();
+    this.deferreds = [];
+
+    this.post = sinon.spy(function() {
+        var deferred = Q.defer();
+        this.deferreds.push(deferred);
+        return deferred.promise;
+    });
+
+    this.resolveAll = function(values) {
+        for (var i in values) {
+            this.resolve(values[i]);
+        }
+    };
+
+    this.resolve = function(value) {
+        if (!this.deferreds.length) {
+            throw new Error("No more deferreds to resolve " + JSON.stringify(value));
+        }
+
+        var deferred = this.deferreds.shift();
+        deferred.resolve(value);
+    };
+
+    this.verifyNoPendingRequests = function() {
+        if (this.deferreds.length) {
+            throw new Error("" + this.deferreds.length + " unresolved requests remaining.");
+        }
+    };
 };
 
 module.exports = HttpMock;
